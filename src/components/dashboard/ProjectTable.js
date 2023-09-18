8e46fc49c974d3e96ce6cbcb95d87e50ac7b37b0
KS7171 <koustub.12@gmail.com>
2023-09-18 22:08:52 +0530

    First Commit

---
+++ src/components/dashboard/ProjectTable.js
@@ -0,0 +1,117 @@
+import { Card, CardBody, CardTitle, CardSubtitle, Table } from "reactstrap";
+import user1 from "../../assets/images/users/user1.jpg";
+import user2 from "../../assets/images/users/user2.jpg";
+import user3 from "../../assets/images/users/user3.jpg";
+import user4 from "../../assets/images/users/user4.jpg";
+import user5 from "../../assets/images/users/user5.jpg";
+import "../../assets/css/ProjectTable.css";
+const tableData = [
+  {
+    avatar: user1,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Flexy React",
+    status: "pending",
+    weeks: "13-09-2023",
+    Technology: "React.js,HTML,CSS",
+  },
+  {
+    avatar: user2,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Lading pro React",
+    status: "Holt",
+    weeks: "13-09-2023",
+    Technology: "React.js,HTML,CSS",
+  },
+  {
+    avatar: user3,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Elite React",
+    status: "holt",
+    weeks: "13-09-2023",
+    Technology: "React.js,HTML,CSS",
+  },
+  {
+    avatar: user4,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Flexy React",
+    status: "pending",
+    weeks: "13-09-2023",
+    Technology: "React.js,HTML,CSS",
+  },
+  {
+    avatar: user5,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Ample React",
+    status: "done",
+    weeks: "13-09-2023",
+    Technology: "React.js,HTML,CSS",
+  },
+];
+
+const ProjectTables = () => {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h5">Project Listing</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Overview of the projects
+          </CardSubtitle>
+
+          <Table className="no-wrap mt-3 align-middle" responsive borderless>
+            <thead>
+              <tr>
+                <th>Team Lead</th>
+                <th>Project Name</th>
+
+                <th>Status</th>
+                <th>Date</th>
+                <th>Technology</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((tdata, index) => (
+                <tr key={index} className="border-top">
+                  <td>
+                    <div className="d-flex align-items-center p-2">
+                      <img
+                        src={tdata.avatar}
+                        className="rounded-circle"
+                        alt="avatar"
+                        width="45"
+                        height="45"
+                      />
+                      <div className="ms-3">
+                        <h6 className="mb-0">{tdata.name}</h6>
+                        <span className="text-muted">{tdata.email}</span>
+                      </div>
+                    </div>
+                  </td>
+                  <td><a href="www.google.com">{tdata.project}</a></td>
+                  <td>
+                    {tdata.status === "pending" ? (
+                      <span className="p-2 bg-danger rounded-circle d-inline-block ms-3"></span>
+                    ) : tdata.status === "holt" ? (
+                      <span className="p-2 bg-warning rounded-circle d-inline-block ms-3"></span>
+                    ) : (
+                      <span className="p-2 bg-success rounded-circle d-inline-block ms-3"></span>
+                    )}
+                  </td>
+                  <td>{tdata.weeks}</td>
+                  <td>{tdata.Technology}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default ProjectTables;
---
+++ src/layouts/FullLayout.js
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import { Container } from "reactstrap";
+
+const FullLayout = () => {
+  return (
+    <main>
+      <div className="pageWrapper d-lg-flex">
+
+
+        <div className="contentArea">
+          {/********header**********/}
+          <Header />
+          {/********Middle Content**********/}
+          <Container className="p-4 wrapper" fluid>
+            <Outlet />
+          </Container>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default FullLayout;
